dcccd472231f9aecaca6f020130e7aa33f785eb6
ersagidullin <ersagidullin@gmail.com>
2026-02-28 17:40:39 +0300

    add new models

--- src/models.py
+++ src/models.py
@@ -1,6 +1,5 @@
-from sqlalchemy import Integer, Text, DateTime, String, JSON
-from sqlalchemy.orm import DeclarativeBase
-from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+from sqlalchemy import Integer, Text, DateTime, String, JSON, ForeignKey, Boolean
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, relationship
 from datetime import datetime
 
 
@@ -20,7 +19,7 @@ class Repository(Base):
     * size: размер репозитория в килобайтах
     * topics: список топиков репозитория
     * pushed_at: дата последнего push
-    * contributors_count: количество контрибьюторов
+    * user_repositories: связи с пользователями
     """
 
     __tablename__ = "repositories"
@@ -36,4 +35,52 @@ class Repository(Base):
     size: Mapped[int] = mapped_column(Integer)
     topics: Mapped[list[str]] = mapped_column(JSON)
     pushed_at: Mapped[datetime] = mapped_column(DateTime)
-    contributors_count: Mapped[datetime] = mapped_column(DateTime)
+
+    user_repositories: Mapped[list["UserRepository"]] = relationship(
+        "UserRepository", back_populates="repository", cascade="all, delete-orphan"
+    )
+
+
+class UserRepository(Base):
+    """
+    * user_id: id пользователя
+    * repository_id: id репозитория
+    * user: объект пользователя
+    * repository: объект репозитория
+    * is_owner: True - владелец; False - contributor
+    """
+
+    __tablename__ = "user_repositories"
+
+    user_id: Mapped[int] = mapped_column(ForeignKey("users.id"), primary_key=True)
+    repository_id: Mapped[int] = mapped_column(
+        ForeignKey("repositories.id"), primary_key=True
+    )
+    user: Mapped["User"] = relationship("User", back_populates="user_repositories")
+    repository: Mapped["Repository"] = relationship(
+        "Repository", back_populates="user_repositories"
+    )
+
+    is_owner: Mapped[bool] = mapped_column(Boolean)
+
+
+class User(Base):
+    """
+    * user_repositories: связи с репозиториями
+    * login: логин
+    * location: локация
+    * company: компания
+    * is_organization: True - организация; False - пользователь
+    """
+
+    __tablename__ = "users"
+
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    user_repositories: Mapped[list["UserRepository"]] = relationship(
+        "UserRepository", back_populates="user", cascade="all, delete-orphan"
+    )
+
+    login: Mapped[str] = mapped_column(String)
+    location: Mapped[str] = mapped_column(String)
+    company: Mapped[str] = mapped_column(String)
+    is_organization: Mapped[bool] = mapped_column(Boolean)
